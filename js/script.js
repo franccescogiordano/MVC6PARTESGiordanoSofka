5b3f3cdf963d9942f5a2eebca2447444e7b515a5
francheskito <franccesco.giordano11@gmail.com>
2022-02-07 14:13:09 -0300

    creacion de canvas y variables, funciones globales

---
+++ js/script.js
@@ -0,0 +1,37 @@
+(function(){
+    self.Board = function(width,heigth){
+        this.width = width;
+        this.heigth = heigth;
+        this.playing = false;
+        this.game_over = false;
+        this.bars = [];
+        this.ball = null;
+    }
+
+    self.Board.prototype = {
+        get elements(){
+            var elements = this.bars;
+            elements.push(ball);
+            return elements;
+        }
+    }
+})();
+
+
+(function(){
+    self.BoardView=function(canvas,board){
+        this.canvas=canvas;
+        this.canvas.width=board.width;
+        this.canvas.height=board.height;
+        this.board=board;
+        this.ctx=canvas.getContext("2d");
+    }
+    })();
+
+    window.addEventListener("load",main);
+function main(){
+var board = new board (800,400);
+var canvas= document.getElementById("canvas",board);
+var board_view= new BoardView();
+}
+
