48a761aa5574b11c3c80057527ad5566c21fc07b
francheskito <franccesco.giordano11@gmail.com>
2022-02-07 20:30:39 -0300

    creacion de objeto pelota y atributos, mas instancia de la misma

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
 (function(){
-    self.Board = function(width,height){
+    self.Board = function(width,height){ //OBJETO ESCENARIO
         this.width = width;
         this.height = height;
         this.playing = false;
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing= false;
     }
 
-    self.Board.prototype = {
+    self.Board.prototype = { //PROTOTIPO ESCENARIO
         get elements(){
-            var elements = this.bars;
+            var elements = this.bars.map(function(bar){return bar;}); //cambio para no llenar de basura la memoria
             elements.push(this.ball);
             return elements;
         }
     }
 })();
+//-------------------------PELOTA--------------------------------
 (function(){
-self.Ball= function(x,y,radius,board){
+self.Ball= function(x,y,radius,board){ //OBJETO PELOTA
     this.x = x;
     this.y = y;
     this.radius = radius;
@@ -26,10 +28,22 @@ self.Ball= function(x,y,radius,board){
     this.board = board;
     board.ball=this;
     this.kind="circle";
+
+
+
+}
+self.Ball.prototype = { //PROTOTIPO PELOTA
+    move: function(){
+        this.x +=(this.speed_y * this.direction);
+        this.y +=(this.speed_x * this.direction);
+    }
 }
 })();
+//-------------------------PELOTA FIN--------------------------------
+
+//-------------------------BARRAS------------------------------------
 (function(){ //funcion para crear las barras
-    self.Bar=function(x,y,width,height,board){
+    self.Bar=function(x,y,width,height,board){ //OBJETO BARRAS
      this.x=x;
      this.y=y;
      this.width=width;
@@ -49,7 +63,9 @@ self.Ball= function(x,y,radius,board){
         }
     }
     })();
+//-------------------------BARRAS FIN------------------------------------
 
+//-------------------------ESCENARIO------------------------------------
 (function(){
     self.BoardView=function(canvas,board){
         this.canvas=canvas;
@@ -59,7 +75,7 @@ self.Ball= function(x,y,radius,board){
         this.ctx=canvas.getContext("2d");
     }
 
-    self.BoardView.prototype = {
+    self.BoardView.prototype = { //BOARD PROTOTIPO
         clean:function(){
             this.ctx.clearRect(0,0,this.board.width,this.board.height);
         },
@@ -70,11 +86,16 @@ self.Ball= function(x,y,radius,board){
             };
        },
        play: function(){
+    if(this.board.playing){
         this.clean();
         this.draw();
-       }
+        this.board.ball.move();
+    }
+  }
    }
-   
+//-------------------------ESCENARIO FIN------------------------------------   
+
+//-------------------------DIBUJOS------------------------------------
    function draw(ctx,element){
        // if(element !== null && element.hasOwnProperty("kind")){
             switch(element.kind){
@@ -91,6 +112,8 @@ self.Ball= function(x,y,radius,board){
      //   } 
     }
     })();
+
+    //CREACION E INSTANCIA DE OBJETOS
     var board = new Board (800,400);
     var bar = new Bar(20,100,40,100,board);
     var bar2 = new Bar(740,100,40,100,board);
@@ -100,22 +123,32 @@ self.Ball= function(x,y,radius,board){
    
     
 document.addEventListener("keydown",function(ev){
-    ev.preventDefault();
+
     if(ev.keyCode == 38){
+        ev.preventDefault();
         bar.up();
     }else if(ev.keyCode == 40){
+        ev.preventDefault();
         bar.down();
     }else if(ev.keyCode == 87){//w
+        ev.preventDefault();
         bar2.up();
     }else if(ev.keyCode == 83){//s
-        bar2.down();
-    }else{
         ev.preventDefault();
+        bar2.down();
+    }else if(ev.keyCode == 32){//PAUSA CON SPACE
+       ev.preventDefault();
+       board.playing=!board.playing;
     }
 });
 
-window.requestAnimationFrame(controller);
 
+
+window.requestAnimationFrame(controller);
+board_view.draw();
+setTimeout(function(){
+ball.direction = -1;
+});
 function controller(){
     board_view.play();
     window.requestAnimationFrame(controller);
